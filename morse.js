7e4c82844fd9a23bfaed61943877900a696e4746
Bogdan Niculescu <Bogdan.Niculescu@Nology.local>
2020-02-28 11:06:04 +0000

    morse code with the reverse input

--- morse.js
+++ morse.js
@@ -52,41 +52,103 @@ const morseCode = {
     "?":"..--..",
 };
 
-const translate = list => {
-    return list.split("").map(convertToMorse).join(" ");
+const englishCode = {
+    "._":"a",
+    "_...":"b",
+    "_._.":"c",
+    "_..":"d",
+    ".":"e",
+    ".._.":"f",
+    "__.":"g",
+    "....":"h",
+    "..":"i",
+    ".___":"j",
+    "_._":"k",
+    "._..":"l",
+    "__":"m",
+    "_.":"n",
+    "___":"o",
+    ".__.":"p",
+    "__._":"q",
+    "._.":"r",
+    "...":"s",
+    "_":"t",
+    ".._":"u",
+    "..._":"v",
+    ".__":"w",
+    "_.._":"x",
+    "_.__":"y",
+    "__..":"z",
+    "_____":"0",
+    ".____":"1",
+    "..___":"2",
+    "...__":"3",
+    "...._":"4",
+    ".....":"5",
+    "_....":"6",
+    "__...":"7",
+    "___..":"8",
+    "____.":"9",
+    "       ":" ",
+    ".-...":"&",
+    ".----.":"'",
+    ".--.-.":"@",
+    "-.--.-":")",
+    "-.--.":"(",
+    "---...":":",
+    "--..--":",",
+    "-...-":"=",
+    "-.-.--":"!",
+    ".-.-.-":".",
+    "-....-":"-",
+    ".-.-.":"+",
+    ".-..-.":'"',
+    "..--..":"?"
+};
+
+const translate = string => {
+
+    return string.split("").map(convertToMorse).join(" ");
 }
 
 const convertToMorse = letter => {
     return morseCode[letter];
 }
 
+// reverse mode
+
+const translateMorse = morse => {
+    return morse.split(" ").map(convertToEnglish).join("");
+}
+
+const convertToEnglish = word => {
+    return englishCode[word];
+}
+
+
 const writeOutput = () => {
     let input = document.getElementById("input").value.toLowerCase();
-    document.getElementById("output").value = translate(input);
+
+    if (input[0].includes(".") || (input.includes("_"))) {
+        document.getElementById("output").value = translateMorse(input);
+    } else {
+        document.getElementById("output").value = translate(input);
+    }
 }
 
 document.getElementById("submit").addEventListener("click", writeOutput);
 
 
 
-
-const changeToWords = morseLetter => {
-    return Object.keys(morseCode).find(key => morseCode[key] === morseLetter);
-}
-
-const translateInput = () => {
-    document.getElementById("output").innerHTML = "";
-    //split input separated by spaces into an array
-    const getInput = document.getElementById("input").value.toLowerCase().split(" ");
+// const translateInput = () => {
+//     document.getElementById("output").innerHTML = "";
+//     //split input separated by spaces into an array
+//     const getInput = document.getElementById("input").value.toLowerCase().split(" ");
     
     //check to see if input is morse. If so run function to translate
-    if (getInput[0].includes(".") || (getInput.includes("_"))) {
-        const convertedMorse = getInput.map(changeToWords);
-        document.getElementById("output").innerHTML = convertedMorse.join("");
-    }
     
-    else {
-        const changeToMorse = getInput.map(convertWord);
-        document.getElementById("output").innerHTML = changeToMorse.join("");
-    }
-}
+//     else {
+//         const changeToMorse = getInput.map(convertWord);
+//         document.getElementById("output").innerHTML = changeToMorse.join("");
+//     }
+// }
